c0faf7f3e54840c3322dc7f36878915bd90290f4
IT-alamm <ma9788181@gmail.com>
2025-09-17 11:01:48 +0530

    adding collections class

---
+++ First.java
@@ -0,0 +1,32 @@
+// package Collection.UtilityClass;
+
+import java.util.*;
+
+public class First {
+
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<>();
+
+        l1.add(18);
+        l1.add(28);
+        l1.add(13);
+        l1.add(19);
+        l1.add(37);
+        l1.add(48);
+
+        // sorting as in Ascending order  using simple method
+        // System.out.println("Before Sorting : "+l1);
+        // Collections.sort(l1);
+        // System.out.println("After sorting : "+l1);
+
+        // sorting as in Descending order by using comparator 
+        // System.out.println("Before Sorting : "+l1);
+        // Collections.sort(l1, (a,b)-> b-a);
+        //  System.out.println("After sorting : "+l1);
+
+
+
+
+    }
+    
+}
